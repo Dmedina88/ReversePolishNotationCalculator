9c01632d7405ca02a0364140bcc8ebc3d19aa8b4
Dmedina88 <dmedina@applicoinc.com>
2015-12-09 13:51:55 -0500

    first commit
    first

---
+++ app/src/main/java/grayherring/reversepolishnotationcalculator/util/Calculator.java
@@ -0,0 +1,16 @@
+package grayherring.reversepolishnotationcalculator.util;
+
+/**
+ * Created by David on 12/9/2015.
+ */
+public class Calculator {
+
+
+   static public String calculat(String rawEquation){
+       String cleanEquation = rawEquation.replace(" ","");
+
+
+       return null;
+   }
+
+}
