eda7545ef89e5992abbc4c716c5ab3e00a23c632
Dmedina88 <dmedina@applicoinc.com>
2015-12-09 17:34:12 -0500

    Fixed adding and subtracting added tests
    Fixed adding and subtracting added tests

--- app/src/androidTest/java/grayherring/reversepolishnotationcalculator/util/CalculatorTest.java
+++ app/src/androidTest/java/grayherring/reversepolishnotationcalculator/util/CalculatorTest.java
@@ -7,14 +7,16 @@ import junit.framework.TestCase;
  */
 public class CalculatorTest extends TestCase {
 
-    public void testCalculat() throws Exception {
-        assertEquals("test 1","0.0",Calculator.calculate("2 3 + 5 + 10 -"));
-        assertEquals("test 2", "11.0",Calculator.calculate("2 2 2 * * 3 +"));
 
-        assertEquals("test 3","-9.0",Calculator.calculate("2 3 + 5 / 10 -"));
-        assertEquals("test 4", "11.0",Calculator.calculate("2 2 2 * * 3 +"));
-        assertEquals("test 5","0.0",Calculator.calculate("2 3 + 5 + 10 -"));
-        assertEquals("test 6", "11.0",Calculator.calculate("2 2 2 * * 3 +"));
+    public void testCalculate() throws Exception {
+        assertEquals("subtract 1","5",Calculator.calculate("10 5 -"));
+        assertEquals("subtract 2","-5",Calculator.calculate("5 10 -"));
+        assertEquals("test 1","0",Calculator.calculate("2 3 + 5 + 10 -"));
+        assertEquals("test 2", "11",Calculator.calculate("2 2 2 * * 3 +"));
+        assertEquals("test 3","-8",Calculator.calculate("2 3 + 2.5 / 10 -"));
+        assertEquals("test 4", "-0.05",Calculator.calculate("10 200 400  -  /"));
+        assertEquals("test 5","1",Calculator.calculate("-2 3 +"));
+       assertEquals("test 6", "2.6666666667",Calculator.calculate("2 2 2 * * 3 /"));
 
     }
 }
--- app/src/main/java/grayherring/reversepolishnotationcalculator/util/Calculator.java
+++ app/src/main/java/grayherring/reversepolishnotationcalculator/util/Calculator.java
@@ -3,6 +3,7 @@ package grayherring.reversepolishnotationcalculator.util;
 import android.util.Log;
 import android.widget.Switch;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Stack;
 
@@ -11,7 +12,7 @@ import java.util.Stack;
  */
 public class Calculator {
 
-
+    private static final String decimalFormatStr =  "0.##########";
    static public String calculate(String rawEquation) throws Exception {
        String delimiter = "[ ]+";
        String[] tokens = rawEquation.split(delimiter);
@@ -22,21 +23,20 @@ public class Calculator {
             if(operators.contains(token)){
                 switch(token){
                     case "+":
-                        stack.add(stack.pop() +stack.pop());
+                        stack.add(stack.pop() + stack.pop());
                         break;
                     case "-":
-                        stack.add(stack.pop() -stack.pop());
+                        stack.add(subtract(stack.pop(), stack.pop()));
                         break;
                     case "*":
-                        stack.add(stack.pop() *stack.pop());
+                        stack.add(stack.pop() * stack.pop());
                         break;
                     case "/":
-                        stack.add(stack.pop() /stack.pop());
+                        stack.add(divide(stack.pop(), stack.pop()));
                         break;
                     default:
                         throw new Exception(" not a valid Argument in Equation");
                 }
-
             }else {
                 try{
                 stack.add(Double.parseDouble(token));
@@ -45,12 +45,15 @@ public class Calculator {
                 }
             }
        }
-
-
-       return stack.pop().toString();
+       DecimalFormat decimalFormat = new DecimalFormat(decimalFormatStr);
+       return decimalFormat.format(stack.pop());
    }
 
-
-
+    public static Double subtract(Double val1,Double val2){
+        return val2 - val1;
+    }
+    public static Double divide(Double val1,Double val2){
+        return val2 / val1;
+    }
 
 }
