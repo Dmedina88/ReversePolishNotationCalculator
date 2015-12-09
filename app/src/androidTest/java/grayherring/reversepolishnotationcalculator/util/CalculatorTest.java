9fb9601a2dec74522e61041e6f799160ba37767d
Dmedina88 <dmedina@applicoinc.com>
2015-12-09 18:32:41 -0500

    Exceptions
    Exceptions

--- app/src/androidTest/java/grayherring/reversepolishnotationcalculator/util/CalculatorTest.java
+++ app/src/androidTest/java/grayherring/reversepolishnotationcalculator/util/CalculatorTest.java
@@ -16,7 +16,32 @@ public class CalculatorTest extends TestCase {
         assertEquals("test 3","-8",Calculator.calculate("2 3 + 2.5 / 10 -"));
         assertEquals("test 4", "-0.05",Calculator.calculate("10 200 400  -  /"));
         assertEquals("test 5","1",Calculator.calculate("-2 3 +"));
-       assertEquals("test 6", "2.6666666667",Calculator.calculate("2 2 2 * * 3 /"));
+        assertEquals("test 6", "2.6666666667",Calculator.calculate("2 2 2 * * 3 /"));
+    }
+
+
+
+    public void testCalculateException() throws Exception {
+        try {
+            Calculator.calculate("2 3 + 5 + 10 - -");
+            fail( "Missing exception" );
+        } catch( IllegalArgumentException e ) {
+        }
+        try {
+            Calculator.calculate("2 2");
+            fail( "Missing exception" );
+        } catch( IllegalArgumentException e ) {
+        }
+        try {
+            Calculator.calculate("1 1 2 0 * / +");
+           fail( "Missing exception" );
+        } catch( ArithmeticException e ) {
+        }
+        try {
+            Calculator.calculate("2 + 2  +");
+            fail( "Missing exception" );
+        } catch( IllegalArgumentException e ) {
+        }
 
     }
 }
--- app/src/main/java/grayherring/reversepolishnotationcalculator/util/Calculator.java
+++ app/src/main/java/grayherring/reversepolishnotationcalculator/util/Calculator.java
@@ -5,7 +5,10 @@ import android.widget.Switch;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 import java.util.Stack;
+
+import javax.xml.transform.Result;
 
 /**
  * Created by David on 12/9/2015.
@@ -13,11 +16,12 @@ import java.util.Stack;
 public class Calculator {
 
     private static final String decimalFormatStr =  "0.##########";
-   static public String calculate(String rawEquation) throws IllegalArgumentException {
+   static public String calculate(String rawEquation) throws IllegalArgumentException,ArithmeticException  {
        String delimiter = "[ ]+";
        String[] tokens = rawEquation.split(delimiter);
        Stack<Double> stack = new Stack<>();
        String operators = "+-*/";
+       try {
        for(String token : tokens){
            Log.i("calculate",token);
             if(operators.contains(token)){
@@ -46,14 +50,32 @@ public class Calculator {
             }
        }
        DecimalFormat decimalFormat = new DecimalFormat(decimalFormatStr);
-       return decimalFormat.format(stack.pop());
+           String result = decimalFormat.format(stack.pop());
+           Log.d("calculate",result);
+           if(stack.isEmpty()){
+               Log.d("calculate",result);
+               return  result;
+           }else {
+               throw  new IllegalArgumentException(" Malformed Input");
+           }
+       }catch (EmptyStackException e){
+           throw new IllegalArgumentException(" Malformed Input");
+       }catch (ArithmeticException e){
+            throw e;
+       }
+
    }
 
-    public static Double subtract(Double val1,Double val2){
+    public static Double subtract(Double val1,Double val2) throws ArithmeticException {
         return val2 - val1;
     }
     public static Double divide(Double val1,Double val2){
-        return val2 / val1;
+        Double result =val2 / val1;
+        if(result.isInfinite()){
+            throw new ArithmeticException("Arithmetic results in dividing by 0");
+        }
+        return result;
+
     }
 
 }
